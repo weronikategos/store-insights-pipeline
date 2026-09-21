9459f6c68c287e90a47ba02974bcf7aa044cfa45
werate <104655767+weronikategos@users.noreply.github.com>
2026-09-21 15:04:54 +0000

    Terraform: outputs and sample configuration

---
+++ infra/outputs.tf
@@ -0,0 +1,19 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "function_app_name" {
+  value = azurerm_linux_function_app.main.name
+}
+
+output "function_app_hostname" {
+  value = azurerm_linux_function_app.main.default_hostname
+}
+
+output "sql_server_fqdn" {
+  value = azurerm_mssql_server.main.fully_qualified_domain_name
+}
+
+output "sql_database_name" {
+  value = azurerm_mssql_database.main.name
+}
